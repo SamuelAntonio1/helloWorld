72d5719207784cc22d5ebf025579fa7df9f72d5c
Renan Molina <renanrobertomolina@hotmail.com>
2023-08-17 20:35:40 -0300

    Aula-17-08
    Array e objetos.

--- src/app/tenis/tenis.page.ts
+++ src/app/tenis/tenis.page.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tenis.page.scss'],
 })
 export class TenisPage implements OnInit {
+
+  listaProdutos = [
+    {nome: "AUTHENTIC",
+    descricao: "Cor: VINHO",
+    valor: 400,
+    },
+
+    {nome: "ERA",
+    descricao: "Cor: Preto",
+    valor: 350
+    },
+
+    {nome: "OLD SKOOL",
+    descricao: "Cor: AZUL",
+    valor: 450
+    },
+
+    {nome: "SLIP-ON",
+    descricao: "Cor: BRANCO",
+    valor: 500
+    },
+
+    {nome: "SK8-HI",
+    descricao: "Cor: PRETO",
+    valor: 350
+    }
+  ];
 
   constructor() { }
 
